adc1fdd877c4c06f9fae7666a05eb207df01cd3e
CodeDotJS <rushforlinux@gmail.com>
2016-05-09 17:58:55 +0530

    passed

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import fn from './';
 
 test(async t => {
-	const tweeder = await fn('twifo');
+	const tweeder = await fn('Rishi_Giri_');
 
-	t.is(tweeder.username, 'pakalupapito');
+	t.is(tweeder.tweet0, 'Start by doing what\'s necessary; then do what\'s possible; and suddenly you are doing the impossible.');
 });
